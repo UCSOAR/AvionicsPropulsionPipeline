66269ba28151126bf544ddbbcb79aa995b39815a
Connell Reffo <connell.reffo@ucalgary.ca>
2025-03-22 11:43:24 -0600

    Dropdown works (#17)

--- web/src/lib/components/Dropdown.svelte
+++ web/src/lib/components/Dropdown.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { ChevronDown } from "@lucide/svelte";
+  import { Check, CheckCheck, ChevronDown, ChevronUp } from "@lucide/svelte";
+  import { onMount } from "svelte";
 
   export let id: string;
   export let label: string | null = null;
@@ -9,16 +10,63 @@
   if (!options) {
     throw new Error("At least one option must be provided.");
   }
+
+  let selectedOptionIndex = 0;
+  let isOptionsVisible = false;
+
+  const handleSelect = (index: number) => {
+    selectedOptionIndex = index;
+    isOptionsVisible = false;
+    onChange(index);
+  };
+
+  const toggleOptions = () => {
+    isOptionsVisible = !isOptionsVisible;
+  };
+
+  onMount(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      const container = document.querySelector(`div.container[id="${id}"]`);
+
+      if (!container?.contains(target)) {
+        isOptionsVisible = false;
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  });
 </script>
 
 <div class="container" {id}>
   {#if label}
     <label for={id}>{label}</label>
   {/if}
-  <button>
-    <span>{options[0]}</span>
-    <ChevronDown />
+  <button class="dropdown-button" on:click={toggleOptions}>
+    <span>{options[selectedOptionIndex]}</span>
+    {#if !isOptionsVisible}
+      <ChevronDown />
+    {:else}
+      <ChevronUp />
+    {/if}
   </button>
+  <ul class="options-container" class:visible={isOptionsVisible}>
+    {#each options as option, index}
+      <li>
+        <button
+          type="button"
+          class:selected={index === selectedOptionIndex}
+          on:click={() => handleSelect(index)}
+        >
+          {option}
+          {#if index === selectedOptionIndex}
+            <Check />
+          {/if}
+        </button>
+      </li>
+    {/each}
+  </ul>
 </div>
 
 <style scoped lang="scss">
@@ -28,13 +76,14 @@
     display: flex;
     flex-direction: column;
     gap: 0.3rem;
+    position: relative;
 
     label {
       white-space: nowrap;
     }
 
-    button {
-      background-color: $bg-color-4;
+    button.dropdown-button {
+      background-color: $bg-color-6;
       border-radius: $border-radius-1;
       padding: 0.55rem;
       font-size: 0.95rem;
@@ -57,6 +106,61 @@
         :global(.lucide-icon) {
           opacity: 1;
         }
+      }
+    }
+
+    ul.options-container {
+      display: none;
+      list-style: none;
+      position: absolute;
+      box-sizing: border-box;
+      background-color: $bg-color-6;
+      z-index: 100;
+      padding: 0.5rem;
+      gap: 0.3rem;
+      margin: 0;
+      margin-top: 0.5rem;
+      border-radius: $border-radius-1;
+
+      & > li {
+        width: 100%;
+
+        * {
+          transition: all 0.1s ease;
+        }
+
+        & > button {
+          display: flex;
+          flex-direction: row;
+          border-radius: $border-radius-1 * 0.7;
+          background: none;
+          padding: 0.6rem;
+          width: 100%;
+
+          :global(.lucide-icon) {
+            width: 1rem;
+            height: auto;
+            stroke: $txt-color-1;
+            margin-left: auto;
+          }
+
+          &:hover {
+            background-color: $bg-color-highlighted;
+            color: $txt-color-highlighted;
+
+            :global(.lucide-icon) {
+              stroke: $txt-color-highlighted;
+            }
+          }
+        }
+      }
+
+      &.visible {
+        display: flex;
+        flex-direction: column;
+        top: 100%;
+        width: 100%;
+        left: 0;
       }
     }
   }
