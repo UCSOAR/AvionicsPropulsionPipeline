a2cf7e4a97110d2367bd9ef88d7a4467e6c7e6f2
kaileykobs <kailey.kobar1@ucalgary.ca>
2025-03-22 11:12:47 -0500

    Kailey/topbar (#14)
    * Made some QOL changes to the frontend
    
    * Added a new Dashboard implementation for comparison purposes.
    
    * cleanup
    
    * hopefully helped with dependencies
    
    * Added svelte files
    
    * Added color variavles
    
    * More colors + icons dep
    
    * Added icon button component
    
    * added a top bar
    
    * got rid of absolute positioning, removed unused imports, made the header a flex container, and changed units to be all in rem
    
    ---------
    
    Co-authored-by: Aly Masani <alymohammed.masani@ucalgary.ca>
    Co-authored-by: connellr023 <connell.reffo@ucalgary.ca>
    Co-authored-by: Kailey <kaileykobar@gmail.com>

---
+++ web/src/lib/components/TopBar.svelte
@@ -0,0 +1,53 @@
+<script lang="ts"></script>
+
+<header class="top-bar">
+  <div class="logo">
+    <img draggable={false} src="/soar-logo.svg" alt="SOAR Logo" />
+  </div>
+  <nav class="nav-links">
+    <a href="#">pyTac</a>
+    <a href="#">pyHac</a>
+    <a href="#">mapleleaf</a>
+  </nav>
+</header>
+
+<style scoped lang="scss">
+  @use "../styles/variables.scss" as *;
+
+  .top-bar {
+    display: flex;
+    align-items: center;
+    gap: 1.3rem;
+    padding: 0.7rem;
+    height: auto;
+    background-color: $bg-color-2;
+    border-bottom: 1px solid $outline-color-1;
+  }
+
+  .logo {
+    margin-right: auto; /* Moves logo to the left */
+  }
+
+  .nav-links {
+    display: flex;
+    justify-content: center; /* Centers links inside */
+    flex-grow: 1; /* Allows nav-links to take available space */
+    gap: 2rem;
+  }
+
+  .nav-links a {
+    text-decoration: none;
+    color: $txt-color-2; /* Light text */
+    font-size: 1.1rem;
+    transition: color 0.2s ease;
+  }
+
+  .logo img {
+    height: 3rem; /* Restores original size */
+    width: auto; /* Keeps aspect ratio */
+  }
+
+  .nav-links a:hover {
+    color: $txt-color-highlighted; /* Change color on hover */
+  }
+</style>
