b93a49004b000963a72a7f3284d5e2509079cd24
kaileykobs <kailey.kobar1@ucalgary.ca>
2025-05-08 08:29:39 -0600

    smoothing filters functions (#28)
    * making smoothing filters functions, and creating tests for them
    
    * removed the helper functions as min/max
    
    ---------
    
    Co-authored-by: Kailey <kaileykobar@gmail.com>
    Co-authored-by: connellr023 <connell.reffo@ucalgary.ca>

---
+++ test/filters/guassian_test.go
@@ -0,0 +1,26 @@
+package filter_test
+
+import (
+	"reflect"
+	"soarpipeline/pkg/filters"
+	"testing"
+)
+
+// Test for Gaussian Filter.
+func TestGaussianFilter(t *testing.T) {
+	xRows := []float64{1, 2, 3, 4, 5}
+	yRows := []float64{10, 20, 30, 40, 50}
+
+	sigma := 1.0
+	result := filters.GaussianFilter(xRows, yRows, sigma)
+
+	// Ensure the output length matches the input length.
+	if len(result) != len(yRows) {
+		t.Errorf("GaussianFilter() returned incorrect length: got %d, expected %d", len(result), len(yRows))
+	}
+
+	// Check if the values are smoothed (not the same as original).
+	if reflect.DeepEqual(result, yRows) {
+		t.Errorf("GaussianFilter() did not modify the input data")
+	}
+}
